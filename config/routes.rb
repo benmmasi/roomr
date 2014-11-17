c6158b0ada164d3d6ddaa4cba0f138c97a7b7d5b
Ben Masi <ben.m.masi@gmail.com>
2014-11-17 17:31:22 -0500

    Admins can add new regions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :regions, only: [:new, :create, :show, :index]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+
+  root "regions#index"
 end
